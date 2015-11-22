4725d7da9b5d44ad568b97e3a7c9d2faaaf2ffaa
Nam Ngo <ngon2@students.wwu.edu>
2015-11-22 00:05:25 -0800

    abc

--- app/scripts/app/app.ts
+++ app/scripts/app/app.ts
@@ -4,13 +4,12 @@ module FishCalc {
     export class Apps {
         mount() {
             m.mount(document.getElementById("divTankSize"), <any>Component.LiterToGallon);
-            m.mount(document.getElementById("divKNO3"), <any>Component.NO3);
-           
+            m.mount(document.getElementById("divKNO3"), <any>Component.NO3);           
         }
     }
 }
 
 $(document).ready(() => {
     var app = new FishCalc.Apps();
-    app.mount(); 
+    app.mount();
 });
