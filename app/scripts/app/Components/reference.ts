9e0e69c6981e7cca4a08bd6da69aaa8339dc557f
Nam Ngo <ngon2@students.wwu.edu>
2015-11-21 23:15:40 -0800

    add references for component folder

---
+++ app/scripts/app/Components/reference.ts
@@ -0,0 +1,3 @@
+﻿/// <reference path="CollapsedText/CollapsedText.ts"/>
+/// <reference path="LiterGallonConverter/LiterGallonController.ts"/>
+/// <reference path="NO3Calculator/NO3Controller.ts"/>
