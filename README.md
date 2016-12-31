726d77e4adf4a6e40f11e4b57df6f86a32ecd878
Nam Ngo <ngon2@students.wwu.edu>
2016-12-31 15:35:24 +0700

    move to modules

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Requirement
 1. Webpack: npm install -g webpack
-2. 
+2. npm install webpack-dev-server --global
+3. 
+
+# Development
+1. To run server locally: http-server dist/
+2. To watch changes using webpack: webpack --progress --colors --watch
--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as _ from "lodash";
 
 import { Hello } from "./components/Hello";
 
+var foo = "" + _.add(3, 4);
+console.log(foo);
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
+    <Hello compiler="TypeScript" framework={foo} />,
     
-    document.getElementById("example-NamNgo")
+    document.getElementById("example")
 );
